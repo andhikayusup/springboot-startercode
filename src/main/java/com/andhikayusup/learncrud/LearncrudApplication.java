93275cb7c4da15fe1aead1a9f370220727481093
andhikayusup <andeeka.ym@gmail.com>
2021-09-02 09:18:36 +0700

    Initialize project

---
+++ src/main/java/com/andhikayusup/learncrud/LearncrudApplication.java
@@ -0,0 +1,13 @@
+package com.andhikayusup.learncrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearncrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andhikayusup/learncrud/LearncrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andhikayusup.learncrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearncrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
